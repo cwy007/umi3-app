8573dad8cc3f9ff8f9a4912d771c0c351a93a080
chenweiyan <chanweiyan007@gmail.com>
2021-02-27 20:39:20 +0800

    navigate to page

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { Layout, Menu } from 'antd';
+import { Link } from 'umi';
 import styles from './index.less';
 
 const { Header, Content, Footer } = Layout;
+const menuData = [
+  { route: '/hero', name: '英雄' },
+  { route: '/item', name: '局内道具' },
+  { route: '/summoner', name: '召唤师技能' },
+];
 
 function BasicLayout(props: any) {
+  const {
+    location: { pathname },
+    children,
+  } = props;
+  console.log(pathname);
+
   return (
     <Layout>
       <Header>
@@ -12,17 +24,19 @@ function BasicLayout(props: any) {
         <Menu
           theme="dark"
           mode="horizontal"
-          defaultSelectedKeys={['1']}
+          defaultSelectedKeys={[pathname]}
           style={{ lineHeight: '64px' }}
         >
-          <Menu.Item key="1">英雄</Menu.Item>
-          <Menu.Item key="2">局内道具</Menu.Item>
-          <Menu.Item key="3">召唤师技能</Menu.Item>
+          {menuData.map((menu) => (
+            <Menu.Item key={`${menu.route}`}>
+              <Link to={menu.route}>{menu.name}</Link>
+            </Menu.Item>
+          ))}
         </Menu>
       </Header>
       <Content style={{ padding: '0 50px' }}>
         <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
-          {props.children}
+          {children}
         </div>
       </Content>
       <Footer style={{ textAlign: 'center' }}>
