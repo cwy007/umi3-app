28d99393407b0241806c6b3c96b83c132d1cb693
chenweiyan <chanweiyan007@gmail.com>
2021-03-01 18:18:41 +0800

    config proxy in .umirc.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import { ResponseError } from 'umi-request';
 
 export const request = {
-  prefix: 'https://pvp.qq.com',
+  prefix: '/api',
   errorHandler: (error: ResponseError) => {
+    // 集中处理错误
     console.log(error);
   },
 };
